13993ab463e56a16ac79b2f714e283346270cae4
Sunny995 <51695320+Sunny995@users.noreply.github.com>
2022-08-23 18:52:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "322";
+            Console.WriteLine(s.ToCurrency());
+            
+        }
+    }
+
+    public static class Extension
+    {
+        public static string ToCurrency(this string str)
+        {
+
+            var IsNumeric = int.TryParse(str, out int n);
+            if (IsNumeric)
+            { str = "$" + str; }
+            else { throw new Exception(); }
+            return str;
+        }
+    
+    }
+}
